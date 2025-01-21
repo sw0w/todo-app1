b120301f2a9ad322660d783fd8e58cd9a74020ac
sw0w <makelajasu@gmail.com>
2025-01-21 14:14:28 +0200

    better validation for email, working on 'required' validation

--- src/components/contactView.js
+++ src/components/contactView.js
@@ -8,78 +8,103 @@ import { useFormStatus } from "react-dom";
 const ContactView = () => {
   const [errors, setErrors] = useState({});
   const { pending, data, error } = useFormStatus();
+  const [text, setText] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     handleValidation(name, value);
+    if (name === "message") {
+      setText(e.target.value);
+    }
   };
 
   // Field validation
   const handleValidation = (name, value) => {
     const isEmpty = (v) => v.trim() === "";
     const isTooShort = (v, minLength) => v.length < minLength;
-    const isValidEmail = (v) => /\S+@\S+\.\S+/.test(v);
-
-    /*
-    
-        To do: Fix the function below as in: fix the helpertext error to show up
-        on both of the textfields, instead of only on where the error was.
-
-        add limitations to message length and a submit ratelimit to prevent spam
-        ^^ as if this was a real app
-
-        better interface styling and make it match with the rest of the site.
-
-    */
+    const isTooLong = (v, maxLength) => v.length > maxLength;
+    const isValidEmail = (v) =>
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
 
     const containsUnsupportedCharacters = (v) => /[\d\s\W]/.test(v);
 
     let validationErrors = { ...errors };
 
-    if (isEmpty(value)) {
-      validationErrors[name] = `${name} cannot be empty`;
-    } else if (isTooShort(value, 3) && name !== "message") {
-      validationErrors[name] = `${name} cannot be shorter than 3 letters.`;
-    } else if (name === "email" && !isValidEmail(value)) {
-      validationErrors[name] = `${name} doesn't contain a valid email`;
-    } else if (containsUnsupportedCharacters(value) && name !== "email") {
-      validationErrors[name] = `${name} contains invalid characters.`;
-    } else {
-      delete validationErrors[name];
+    // NAME
+    if (name === "name") {
+      if (isEmpty(value)) {
+        validationErrors.name = "Name cannot be empty";
+      } else if (isTooShort(value, 3)) {
+        validationErrors.name = "Name cannot be shorter than 3 letters";
+      } else if (containsUnsupportedCharacters(value)) {
+        validationErrors.name = "Only letters are allowed";
+      } else {
+        delete validationErrors.name;
+      }
     }
+    // EMAIL
+    if (name === "email") {
+      if (isEmpty(value)) {
+        validationErrors.email = "Email cannot be empty";
+      } else if (!isValidEmail(value)) {
+        validationErrors.email = "Email is not valid";
+      } else {
+        delete validationErrors.email;
+      }
+    }
+
+    // MESSAGE
+    if (name === "message") {
+      if (isTooLong(value, 3000)) {
+        validationErrors.message = "message cannot be over 3000 characters.";
+      } else {
+        delete validationErrors.message;
+      }
+    }
+
     setErrors(validationErrors);
   };
 
-  const submitForm = async (event) => {
-    event.preventDefault();
+  const [Cooldown, setCooldown] = useState(false);
+
+  const submitForm = async (data) => {
+    if (Cooldown) {
+      console.log("Please wait before submitting again.");
+      return;
+    }
 
     const hasErrors = Object.keys(errors).length > 0;
     if (hasErrors) {
       console.log("Errors:", errors);
-    } else {
-      const formData = new FormData(event.target);
-      const formObject = Object.fromEntries(formData.entries());
-      console.log("Form submitted with data:", formObject);
+      return;
+    }
 
-      try {
-        const response = await fetch(
-          "https://dummyjson.com/c/7237-500e-488f-a50c",
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(formObject),
-          }
-        );
+    const formObject = Object.fromEntries(data.entries());
 
-        if (!response.ok) {
-          throw new Error("Failed to submit. Try again later.");
+    try {
+      setCooldown(true);
+
+      setTimeout(() => {
+        setCooldown(false);
+      }, 5000);
+
+      const response = await fetch(
+        "https://dummyjson.com/c/7237-500e-488f-a50c",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formObject),
         }
+      );
 
-        const result = await response.json();
-        console.log("Success:", result);
-      } catch (error) {
-        console.error("Error submitting feedback:", error.message);
+      if (!response.ok) {
+        throw new Error("Failed to submit. Try again later.");
       }
+      console.log("Form submitted with data:", formObject);
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error submitting feedback:", error.message);
     }
   };
 
@@ -88,43 +113,49 @@ const ContactView = () => {
       <Header />
       <Box className="half-background" />
       <div className="wrapper">
-        <div className="contact-container">
-          <Typography variant="h5" className="contact-title">
+        <div className="contact-title-container">
+          <Typography variant="h1" className="contact-title">
             Contact Us
           </Typography>
+          <Typography variant="h5" className="contact-subtitle">
+            We’d love to hear from you!
+          </Typography>
+        </div>
+
+        <div className="contact-container">
           <form
             className="contact-form"
-            onSubmit={(e) => {
+            action={(e) => {
               if (!pending) {
                 submitForm(e);
               }
             }}
           >
+            <div className="name-email-container">
+              <TextField
+                fullWidth
+                label="Name"
+                name="name"
+                variant="standard"
+                className="contact-field"
+                onChange={handleChange}
+                error={!!errors?.name}
+                helperText={errors?.name || "Type your name"}
+              />
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                variant="standard"
+                className="contact-field"
+                onChange={handleChange}
+                error={!!errors?.email}
+                helperText={errors?.email || "Type your email"}
+              />
+            </div>
+
             <TextField
               fullWidth
-              required
-              label="Name"
-              name="name"
-              variant="standard"
-              className="contact-field"
-              onChange={handleChange}
-              error={!!errors?.name}
-              helperText={errors?.name?.message || "Type your name"}
-            />
-            <TextField
-              fullWidth
-              required
-              label="Email"
-              name="email"
-              variant="standard"
-              className="contact-field"
-              onChange={handleChange}
-              error={!!errors?.name}
-              helperText={errors?.name?.message || "Type your email"}
-            />
-            <TextField
-              fullWidth
-              required
               label="Message"
               name="message"
               multiline
@@ -132,7 +163,10 @@ const ContactView = () => {
               variant="standard"
               className="contact-field"
               onChange={handleChange}
+              error={!!errors?.message}
+              helperText={errors?.message || `${text.length}/3000`}
             />
+
             <Button
               variant="contained"
               color="primary"
@@ -148,4 +182,5 @@ const ContactView = () => {
     </>
   );
 };
+
 export default ContactView;
